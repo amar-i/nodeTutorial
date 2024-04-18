0f82ada23b29cfc323db83db0350e5a20dfb88a4
Josh B <joshua.banaga@yahoo.com>
2024-04-18 14:44:33 -0700

    Added some mock data and routes

--- src/app.js
+++ src/app.js
@@ -6,10 +6,44 @@ app.listen(PORT, () => {
     console.log(`Server is listening to ${PORT}...`)
 });
 
+const customers = [
+    {
+        name: "Alice",
+        age: 25,
+        occupation: "Engineer",
+        location: "San Francisco, CA",
+        hobbies: ["cycling", "painting"],
+        description: "Alice is a mechanical engineer who loves to innovate and design new products. She enjoys cycling around the city and spends weekends painting landscapes."
+    },
+    {
+        name: "Bob",
+        age: 30,
+        occupation: "Designer",
+        location: "New York, NY",
+        hobbies: ["photography", "traveling"],
+        description: "Bob is a graphic designer specializing in digital art. He has a passion for photography and travels frequently to capture diverse cultures and landscapes."
+    },
+    {
+        name: "Charlie",
+        age: 28,
+        occupation: "Teacher",
+        location: "Chicago, IL",
+        hobbies: ["reading", "chess"],
+        description: "Charlie teaches history at a high school and loves to engage students with interactive learning. In his free time, he reads historical novels and plays chess."
+    }
+];
+
+// Home route
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Welcome to my test API!')
 });
 
-app.post('/', (req, res) => {
-    res.send('Testing if the post is working')
+// Customers route
+app.get('/api/customers', (req, res) => {
+    res.send({customers: customers})
+});
+
+// Post Customers route
+app.post('/api/customers', (req, res) => {
+    res.json ({customers: customers.map(customer => customer.name)})
 });
