6f8b23756de4ddbd100d6d6e4e6f1c0f3ba9c170
Josh <jbanaga@my.bcit.ca>
2024-04-19 11:32:14 -0700

    POST done!

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const dbURI = process.env.DB_URI;
 const Customer = require('./models/customer');
+const customer = require("./models/customer");
 
 // Express application
 const app = express();
@@ -19,60 +20,59 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Database connection
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 async function connectToDatabase(uri) {
-    try {
-        await mongoose.connect(uri);
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error(`Database connection error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(`Database connection error: ${error.message}`);
+    process.exit(1);
+  }
 }
 
 // Server start function
 async function startServer() {
-    await connectToDatabase(dbURI);
-    app.listen(PORT, () => {
-        console.log(`Server is listening on port ${PORT}...`);
-    });
+  await connectToDatabase(dbURI);
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
+  });
 }
 
-// Customers data
-const  customer = new Customer({
-    name: 'John Doe 2.0',
-    email: 'john@testemail.com',
-    industry: 'Technology',
-    age: 35,
-});
-
 // Routes
-app.get('/', (req, res) => {
-    res.send('Welcome to my test API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to my test API!");
 });
 
 // Get all customers
-app.get('/api/customers', async (req, res) => {
-    try {
-        const customers = await Customer.find();
-        res.json(customers);
-    } catch (e) {
-        res.status(500).json({ Error: e.message });
-    }
+app.get("/api/customers", async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    res.json(customers);
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+  }
+});
+
+// Customers data
+let customer1 = new Customer({
+  name: "Jane Doe",
+  email: "john@testemail.com",
+  industry: "Technology",
+  age: 35,
 });
 
 // Add a new customer
-app.post('/api/customers', (req, res) => {
-    try {
-        const customer = new Customer(req.body);
-        customer.save();
-        res.status(201).json({customer});
-        console.log('Customer saved successfully', customer);
-    } catch (error) {
-        res.status(400).json({ Error: error.message });
-    }
-
+app.post("/api/customers", async (req, res) => {
+  try {
+    const customer = new Customer(req.body);
+    await customer.save();
+    res.status(201).json({ customer });
+    console.log("Customer saved successfully", customer);
+  } catch (error) {
+    res.status(400).json({ Error: error.message });
+  }
 });
 
 // Start the server
